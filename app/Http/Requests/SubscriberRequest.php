a172692ac9f10b85aa86d2bf28e8c649c89514ae
Azer Mamedov <mamedovazer124@gmail.com>
2022-05-16 09:11:59 +0400

    Update SubscriberRequest.php

--- app/Http/Requests/SubscriberRequest.php
+++ app/Http/Requests/SubscriberRequest.php
@@ -11,7 +11,7 @@ class SubscriberRequest extends FormRequest
         return [
             'email' => 'required|email:rfc,dns|max:50|unique:subscribers,email',
             'token' => 'required|string|min:50|max:50|unique:subscribers,token',
-            'locale' => 'required|in:' . implode(',', config('app.locales')),
+            'lang' => 'required|in:' . implode(',', config('app.locales')),
         ];
     }
 }
